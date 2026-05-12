3815e515a9890c140fcf867c562d7595f7eda7b4
LivSarah <LivSarah@users.noreply.github.com>
2026-05-12 01:18:29 +0000

    deploy: ce120d8dc6a75e35cbba9aee7715b11062938060

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { GalleryComponent } from './components/gallery/gallery.component';
-
-export const routes: Routes = [
-  { path: '', component: GalleryComponent },
-  { path: '**', redirectTo: '' }
-];
