ce6f3201686ee16b8f8e1a14d35914e30557a2a1
Liv Sarah Ansnes <37642303+LivSarah@users.noreply.github.com>
2026-07-08 20:42:30 +0200

    Adding new route to display a PDF-file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { GalleryComponent } from './components/gallery/gallery.component';
+import { PdfViewerComponent } from './components/pdf-viewer/pdf-viewer.component';
 
 export const routes: Routes = [
   { path: '', component: GalleryComponent },
+  { path: 'pdf', component: PdfViewerComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/components/pdf-viewer/pdf-viewer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf-viewer',
+  standalone: true,
+  templateUrl: './pdf-viewer.component.html',
+  styleUrl: './pdf-viewer.component.scss'
+})
+export class PdfViewerComponent {}
